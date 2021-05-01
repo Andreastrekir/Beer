9ae9010d6b82a29ff4b4cb64f72b656cf12fd7cc
andreastrekir <astrekir@gmail.com>
2021-05-01 22:20:24 +0300

    First commit

---
+++ beer.js
@@ -0,0 +1,26 @@
+function numberBeer(){
+	userNumber = prompt ("Can you put a number? Please");
+
+for(let i = userNumber; i <= 99; i++) {
+	
+	console.log(`${userNumber} bottles of beer`);
+	console.log(`${userNumber++} bottles of beer on the wall`);
+
+for(let j = 1; j < 2; j++){
+	console.log(`Take ${j+1} down, pass them around`);}
+}
+}
+
+numberBeer();
+
+
+
+
+
+
+
+
+
+
+
+
